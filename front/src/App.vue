f5b6ecc2626527f85281253c136aae02a5f8f145
Celeo <celeodor@gmail.com>
2016-06-15 16:50:55 -0700

    Starting addition of websockets to Vue

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,15 +1,43 @@
 <template>
 <div id="app">
-    <!-- -->
+
 </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import VueSocketio from 'vue-socket.io'
+
+
+Vue.use(VueSocketio, 'http://localhost:5000')
+
 export default {
     data () {
         return {
-            // 
+            //
         }
+    },
+    computed: {
+        //
+    },
+    sockets: {
+        connect: function() {
+            console.log('Connected to the backend web socket server')
+        },
+        disconnect: function() {
+            console.log('Disconnected from the backend web socket server')
+        },
+        event: function(data) {
+            console.log('Got event: ' + data.message)
+
+        },
+        response: function(data) {
+            console.log('Got response: ' + data.message)
+
+        }
+    },
+    methods: {
+        // 
     }
 }
 </script>
